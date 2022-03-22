4c98a29084c92155ef42e04276abe0c901767e68
Vivian Yim <keroviv86@gmail.com>
2022-03-22 13:30:43 -0500

    styled threadCards

--- client/src/components/features/TweetThread/ThreadCard.js
+++ client/src/components/features/TweetThread/ThreadCard.js
@@ -5,14 +5,16 @@ function ThreadCard({thread}){
     return (
         <div  className = "column">
             <div className = "card">
-                <NavLink to={`/thread/${thread["id"]}`}>
-                <h3>{thread["author"]["name"]}</h3>
-                <br/>
-                <h5>{thread["title"]}</h5>
-                <br/>
-                <h6>{thread["description"]}</h6>
-                <br/>
-                <a className="tweet-btn">Read {thread["tweets"].length} tweets</a>
+                <NavLink activeClassName="active" to={`/thread/${thread["id"]}`}>
+                    <h3 className = "author-name">{thread["author"]["name"]}</h3>
+                    <br/>
+                    <br/>
+                    <hr/>
+                    <h5 className="thread-title">{thread["title"]}</h5>
+                    <br/>
+                    <h6 className="thread-description">{thread["description"]}</h6>
+                    <br/>
+                    <a className="tweet-btn">Read {thread["tweets"].length} tweets</a>
                 </NavLink>
             </div>
         </div>
--- client/src/components/features/TweetThread/ThreadList.js
+++ client/src/components/features/TweetThread/ThreadList.js
@@ -1,7 +1,7 @@
 import { React, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
-
+import ThreadCard from "./ThreadCard";
 import { fetchAllThreads } from "./threadSlice";
 
 function ThreadList() {
@@ -16,21 +16,11 @@ function ThreadList() {
   }, []);
 
   const threadsToDisplay = threads.map((thread) => (
-  
-         <div key={thread.id}  className = "column">
-            <div className = "card">
-                <NavLink to={`/thread/${thread["id"]}`}>
-                <h3>{thread["author"]["name"]}</h3>
-                <br/>
-                <h5>{thread["title"]}</h5>
-                <br/>
-                <h6>{thread["description"]}</h6>
-                <br/>
-                <a className="tweet-btn">Read {thread["tweets"].length} tweets</a>
-                </NavLink>
-            </div>
-        </div>
-
+    <ThreadCard 
+     key={thread.id}
+     thread = {thread}
+    
+    />
    
   ));
 
