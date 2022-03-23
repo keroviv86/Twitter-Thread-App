6f495ac1b8f26005082998813d17cb5ff4f0597c
Vivian Yim <keroviv86@gmail.com>
2022-03-22 19:47:27 -0500

    added 3 rspec for tweethread

--- spec/models/tweetthread.rb
+++ spec/models/tweetthread.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Tweetthread, type: :model do
     let(:user) {User.create(name: 'Kevin', email: "keroviv86@gmail.com", password: "turtle", admin: false)}
     let(:tweetthread) {Tweetthread.create(title:'Test Thread #1', description: 'my first thread!', user_id: user.id)}
-    
+    let(:tweet) {Tweet.create(twitter_api_id: '1502673952572854278', tweetthread_id: tweetthread.id, order: 2)}
 
     describe "relationships" do
 
@@ -14,9 +14,11 @@ RSpec.describe Tweetthread, type: :model do
         end
 
         it 'can access the associated user' do
-            comment = Comment.create(user_id: user.id, tweetthread_id: tweetthread.id, parent_comment_id: 0, comment: 'test')
-            
             expect(tweetthread.user).to eq(user)
+        end
+
+        it 'can access the associated tweet' do 
+            expect(tweetthread.tweets).to include(tweet)
         end
     end
 end
