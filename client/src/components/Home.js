40db78ea743d40a9bbe5753cc52c936e45139662
Vivian Yim <keroviv86@gmail.com>
2022-03-25 22:07:23 -0500

    added search bar to thread container

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
-import { React, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import ThreadList from "./features/TweetThread/ThreadList";
 import { fetchAllThreads } from './features/TweetThread/threadSlice';
 import ThreadCard from './features/TweetThread/ThreadCard'
 import { useSelector, useDispatch } from "react-redux";
 
 function Home({ user }) {
+  const [searchThread, setSearchThread] = useState("")
   const threads = useSelector((state) => state.threads.allThreads);
  
   const dispatch = useDispatch();
@@ -13,7 +14,12 @@ function Home({ user }) {
     dispatch(fetchAllThreads())
   },[])
 
-  const threadsToDisplay = threads.map((thread) => (
+  const allFilteredThreads = [...threads]
+  .filter((items) => {
+    return items.title.toLowerCase().includes(searchThread.toLowerCase());
+  })
+
+  const threadsToDisplay = allFilteredThreads.map((thread) => (
     <ThreadCard 
      key={thread.id}
      id={thread.id}
@@ -23,6 +29,15 @@ function Home({ user }) {
   ));
   return (
     <>
+      <form className= "search-box">
+          <input
+          className = "search-input" 
+          type="search"
+          placeholder="Search for a thread..."
+          required
+          onChange={(e)=> setSearchThread(e.target.value)}
+          />
+      </form>
       <div className="card-container">
           {threadsToDisplay}
       </div>
