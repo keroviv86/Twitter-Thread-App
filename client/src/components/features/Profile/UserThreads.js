d0b90d29d8192246fd91fed104caca1f681f162a
Vivian Yim <keroviv86@gmail.com>
2022-03-25 21:53:14 -0500

    linked user profile threads to show individual threads

--- client/src/components/features/Profile/UserThreads.js
+++ client/src/components/features/Profile/UserThreads.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 import "./Profile.css";
+import { NavLink } from "react-router-dom";
 
 function UserThreads({user}) {
 
@@ -9,7 +10,9 @@ function UserThreads({user}) {
             <div className="created_threads">
                 {
                 user["tweetthreads"].map(thread =>(
+                <NavLink activeClassName="active" to={`/thread/${thread["id"]}`}>
                 <li className="create_threads_li">{thread["title"]}</li>
+                </NavLink>
                 ))
                 }
 
